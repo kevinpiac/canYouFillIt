4fec723c5580363a8e588a54142bad7bf9ff2458
Kevin Piacentini <kevinpiac@gmail.com>
2016-03-10 18:46:34 +0100

    Adding read_bufoint (verified)

--- project/src/header.h
+++ project/src/header.h
@@ -6,7 +6,7 @@
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/06 14:37:50 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/03/10 17:15:24 by kpiacent         ###   ########.fr       */
+/*   Updated: 2016/03/10 18:29:31 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,47 +20,47 @@
 **    MOVE.C / FUNCTIONS TO MOVE
 */
 
-void	move_down(unsigned int *t);
-void	move_up(unsigned int *t);
-void	move_left(unsigned int *t);
-void	move_right(unsigned int *t);
-void	move_topleft(unsigned int *t);
+void			move_down(unsigned int *t);
+void			move_up(unsigned int *t);
+void			move_left(unsigned int *t);
+void			move_right(unsigned int *t);
+void			move_topleft(unsigned int *t);
 
 /*
 **    POS.C / FUNCTIONS TO CHECK POSITIONS
 */
 
-int		pos_cmp(unsigned int t1, unsigned int t2);
-int		pos_isfree(unsigned int *tab, unsigned int index, size_t size);
+int				pos_cmp(unsigned int t1, unsigned int t2);
+int				pos_isfree(unsigned int *tab, unsigned int index, size_t size);
 
 /*
 **    DEBUG.C
 */
 
-void	debug_coordinates(unsigned int t);
+void			debug_coordinates(unsigned int t);
 
 /*
 **    PRINT.C
 */
 
-void	print_result(unsigned int *t, unsigned int nbr);
+void			print_result(unsigned int *t, unsigned int nbr);
 
 /*
 **    SOLVE.C
 */
 
-size_t	solve_getsqsize(unsigned int *t, unsigned int nbr);
+size_t			solve_getsqsize(unsigned int *t, unsigned int nbr);
 
 /*
 **    READ.C
 */
 
-void	read_file(char *file, unsigned int *tab);
-
+int				read_file(char *file, unsigned int *tab);
+unsigned int	read_buftoint(char *buf);
 /*
 **    CHECK.c
 */
 
-int		check_buf(char *buf, int size);
+int				check_buf(char *buf, int size);
 
 #endif
--- project/src/read.c
+++ project/src/read.c
@@ -6,14 +6,14 @@
 /*   By: kpiacent <kpiacent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/07 10:03:23 by kpiacent          #+#    #+#             */
-/*   Updated: 2016/03/10 17:15:12 by kpiacent         ###   ########.fr       */
+/*   Updated: 2016/03/10 18:45:14 by kpiacent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "header.h"
 #include <stdio.h>
 
-void	read_file(char *file, unsigned int *tab)
+int				read_file(char *file, unsigned int *tab)
 {
 	int		fd;
 	int		ret;
@@ -29,13 +29,47 @@ void	read_file(char *file, unsigned int *tab)
 			if (check_buf(buf, ret) == -1)
 			{
 				ft_putstr_fd("error\n", 0);
-				return ;
+				return (-1);
 			}
 			(void)tab;
+			ft_putnbr(read_buftoint(buf));
+			ft_putstr("\n");
+			ft_bitprint(read_buftoint(buf));
+
 			size = 21;
 		}
-		//print_error
+		return (1);
 	}
-	//check_buf;
+	return (-1);
 }
 
+unsigned int	read_buftoint(char *buf)
+{
+	int				x;
+	int				y;
+	int				i;
+	unsigned int	t;
+
+	t = 0;
+	x = 0;
+	y = 0;
+	i = 7;
+	while (*buf)
+	{
+		if (*buf == '#')
+		{
+			ft_bitsetfour(&t, x, i);
+			ft_bitsetfour(&t, y, i - 1);
+			i -= 2;
+		}
+		if (*buf == '\n')
+		{
+			x++;
+			y = 0;
+		}
+		else
+			y++;
+		buf++;
+	}
+	return (t);
+}
